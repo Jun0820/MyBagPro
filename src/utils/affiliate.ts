20c6a179b57166f21ed5c924261ebd0d66fefcc2
Jun0820 <tomitajunpei@gmail.com>
2026-04-24 17:02:58 +0900

    Update Amazon affiliate tag

--- src/utils/affiliate.ts
+++ src/utils/affiliate.ts
@@ -1,28 +1,29 @@
 
+const AMAZON_ASSOCIATE_TAG = 'mybagpro-22';
+const RAKUTEN_AFFILIATE_ID = '1f433d4e.d742b51d.1f433d4f.c13174af';
+const YAHOO_VALUECOMMERCE_SID = '3762568';
+const YAHOO_VALUECOMMERCE_PID = '892534722';
+
 export const getAffiliateUrl = (brand: string, modelName: string, shop: 'AMAZON' | 'RAKUTEN' | 'RAKUTEN_GOLF_PARTNER' | 'RAKUTEN_GOLF_PARTNER_ANNEX' | 'YAHOO' = 'RAKUTEN'): string => {
     const query = encodeURIComponent(`${brand} ${modelName}`);
 
     switch (shop) {
         case 'AMAZON':
-            // StoreID: funrecipe0f-22
-            return `https://www.amazon.co.jp/s?k=${query}&tag=funrecipe0f-22`;
+            return `https://www.amazon.co.jp/s?k=${query}&tag=${AMAZON_ASSOCIATE_TAG}`;
         case 'RAKUTEN':
-            // Rakuten Affiliate ID: 1f433d4e.d742b51d.1f433d4f.c13174af
             const target = encodeURIComponent(`https://search.rakuten.co.jp/search/mall/${query}`);
-            return `https://hb.afl.rakuten.co.jp/hgc/1f433d4e.d742b51d.1f433d4f.c13174af/?pc=${target}&m=${target}`;
+            return `https://hb.afl.rakuten.co.jp/hgc/${RAKUTEN_AFFILIATE_ID}/?pc=${target}&m=${target}`;
         case 'RAKUTEN_GOLF_PARTNER':
             // Rakuten Golf Partner (Shop ID: 226919)
-            // Rakuten Affiliate ID: 1f433d4e.d742b51d.1f433d4f.c13174af
             const rgpTarget = encodeURIComponent(`https://search.rakuten.co.jp/search/mall/${query}/?sid=226919`);
-            return `https://hb.afl.rakuten.co.jp/hgc/1f433d4e.d742b51d.1f433d4f.c13174af/?pc=${rgpTarget}&m=${rgpTarget}`;
+            return `https://hb.afl.rakuten.co.jp/hgc/${RAKUTEN_AFFILIATE_ID}/?pc=${rgpTarget}&m=${rgpTarget}`;
         case 'RAKUTEN_GOLF_PARTNER_ANNEX':
             // Rakuten Golf Partner Annex (Shop ID: 285653)
             const annexTarget = encodeURIComponent(`https://search.rakuten.co.jp/search/mall/${query}/?sid=285653`);
-            return `https://hb.afl.rakuten.co.jp/hgc/1f433d4e.d742b51d.1f433d4f.c13174af/?pc=${annexTarget}&m=${annexTarget}`;
+            return `https://hb.afl.rakuten.co.jp/hgc/${RAKUTEN_AFFILIATE_ID}/?pc=${annexTarget}&m=${annexTarget}`;
         case 'YAHOO':
-            // ValueCommerce: sid=3762568, pid=892534722
             const yahooTarget = encodeURIComponent(`https://shopping.yahoo.co.jp/search?p=${query}`);
-            return `http://ck.jp.ap.valuecommerce.com/servlet/referral?sid=3762568&pid=892534722&vc_url=${yahooTarget}`;
+            return `http://ck.jp.ap.valuecommerce.com/servlet/referral?sid=${YAHOO_VALUECOMMERCE_SID}&pid=${YAHOO_VALUECOMMERCE_PID}&vc_url=${yahooTarget}`;
         default:
             return `https://www.google.com/search?q=${query}`;
     }
